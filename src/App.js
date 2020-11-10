4aa2e0eb5fc6441e45f46d01fb57651b8987a986
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-11-10 11:21:22 +0600

    react.js header top and bottom

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/header/header';
+import './base.scss';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
