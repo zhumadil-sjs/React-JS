6a49011db5b250b4492a2fd6b4890af397820ec0
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-11-13 22:17:39 +0600

    footer js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,58 +1,58 @@
-import search from '../../assets/search.svg';
-import phone from '../../assets/phone.svg';
-import cart from '../../assets/cart.svg';
-import user from '../../assets/user.svg';
-import './styles.scss';
-
+import { Link } from "react-router-dom";
+import search from "../../assets/search.svg";
+import phone from "../../assets/phone.svg";
+import cart from "../../assets/cart.svg";
+import user from "../../assets/user.svg";
+import "./styles.scss";
 const Header = () => {
   return (
-    <header className='header'>
-      <div className='container'>
-        <div className='header__top'>
+    <header className="header">
+      <div className="container">
+        <div className="header__top">
           <div>
-            <img className="header__phone" src={phone} alt='phone' />
-            <img src={search} alt='search' />
+            <img className="header__phone" src={phone} alt="phone" />
+            <img src={search} alt="search" />
           </div>
-          <div className='header__logo'>
-            <a href='' className='logo__link'>
+          <div className="header__logo">
+            <a href="" className="logo__link">
               Shoply
             </a>
           </div>
           <div>
-            <img className="header__cart" src={cart} alt='cart' />
-            <img src={user} alt='user' />
+            <img className="header__cart" src={cart} alt="cart" />
+            <img src={user} alt="user" />
           </div>
         </div>
-        <div className='header__bottom'>
-          <nav className='header__nav nav'>
-            <ul className='nav__menu'>
-              <li className='nav__item'>
-                <a href='#hero' className='nav__link'>
+        <div className="header__bottom">
+          <nav className="header__nav nav">
+            <ul className="nav__menu">
+              <li className="nav__item">
+                <Link to="/" className="nav__link">
                   Home
-                </a>
+                </Link>
               </li>
-              <li className='nav__item'>
-                <a href='#ajara' className='nav__link'>
+              <li className="nav__item">
+                <Link to="/products" className="nav__link">
                   New In
-                </a>
+                </Link>
               </li>
-              <li className='nav__item'>
-                <a href='' className='nav__link'>
+              <li className="nav__item">
+                <Link to="/aiza" className="nav__link">
                   Women Product
-                </a>
+                </Link>
               </li>
-              <li className='nav__item'>
-                <a href='' className='nav__link'>
+              <li className="nav__item">
+                <a href="" className="nav__link">
                   Men Product
                 </a>
               </li>
-              <li className='nav__item'>
-                <a href='' className='nav__link'>
+              <li className="nav__item">
+                <a href="" className="nav__link">
                   Top Brands
                 </a>
               </li>
-              <li className='nav__item'>
-                <a href='' className='nav__link'>
+              <li className="nav__item">
+                <a href="" className="nav__link">
                   Special Offers
                 </a>
               </li>
