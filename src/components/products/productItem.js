f36662503c5839467cf7394c2d89f0a1d4b6606e
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-11-15 20:37:02 +0600

    #3

--- src/components/products/productItem.js
+++ src/components/products/productItem.js
@@ -1,20 +1,22 @@
-import productImg from '../../assets/productflower.png';
+import productImg from "../../assets/productflower.png";
 
 function ProductItem(props) {
   if (props.boolean) {
     return (
-      <div className='col-6 products__item'>
+      <div className="col-6 products__item">
         <img src={props.img} />
       </div>
     );
   } else {
     return (
-      <div className='col-6 products__item'>
-        <img src={productImg} alt='' className='products__item__img' />
-        <h2 className='products__item__title title'>{props.title}</h2>
-        <h3 className='products__item__subtitle subtitle'>{props.subtitle}</h3>
-        <p className='products__item__description description'>{props.description}</p>
-        <a href='' className='products__item__btn btn'>
+      <div className="col-6 products__item">
+        <img src={productImg} alt="" className="products__item__img" />
+        <h2 className="products__item__title title">{props.title}</h2>
+        <h3 className="products__item__subtitle subtitle">{props.subtitle}</h3>
+        <p className="products__item__description description">
+          {props.description}
+        </p>
+        <a href="" className="products__item__btn btn">
           View All
         </a>
       </div>
