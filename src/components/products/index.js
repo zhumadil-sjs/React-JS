eb3080f3952d2b02985c50c0dbce829e18310300
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-11-15 20:36:35 +0600

    #2

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ProductItem from './productItem';
-import { obj } from './backend';
+import React from "react";
+import ProductItem from "./productItem";
+import { obj } from "./backend";
 
 const Products = () => {
   return (
-    <section className='products'>
-      <div className='container'>
-        <div className='products__row row'>
+    <section className="products">
+      <div className="container">
+        <div className="products__row row">
           {obj.map((item) => {
             return (
               <ProductItem
