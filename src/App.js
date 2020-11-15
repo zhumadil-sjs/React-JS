3150545363289723a84e17657d96bc5751204ebd
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-11-15 18:58:06 +0600

    app js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { Route, Router } from 'react-router-dom';
-import Header from './components/header';
-import Hero from './components/hero';
-import Products from './components/products';
-import Example from './components/footer';
-import Video from './components/video';
-import './base.scss';
+import { Route, Router } from "react-router-dom";
+import Header from "./components/header";
+import Hero from "./components/hero";
+import Products from "./components/products";
+import Example from "./components/footer";
+import Video from "./components/video";
+import "./base.scss";
 
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       {/* <Header />
       <Hero />
       <Products /> */}
-      <Route exact path='/' component={Hero} />
-      <Route exact path='/products' component={Products} />
-      <Route exact path='/aiza' component={Example} />
-      <Route exact path='/video' component={Video} />
+      <Route exact path="/" component={Hero} />
+      <Route exact path="/products" component={Products} />
+      <Route exact path="/aiza" component={Example} />
+      <Route exact path="/video" component={Video} />
     </div>
   );
 };
--- src/components/video/index.js
+++ src/components/video/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Video() {
-    return (
-        <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/2Vv-BfVoq4g" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-    )
+  return (
+    <div className="video">
+      <iframe
+        width="1265"
+        height="700"
+        src="https://www.youtube.com/embed/2Vv-BfVoq4g"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 }
 
-export default Video
+export default Video;
